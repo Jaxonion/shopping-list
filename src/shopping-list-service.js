e80a72c86a29fd0fc6e08def16c1c160b2ed87a5
Jaxon Potter <jaxonpotter@gmail.com>
2020-04-04 15:07:01 -0600

    update

--- src/shopping-list-service.js
+++ src/shopping-list-service.js
@@ -1,36 +1,35 @@
 const ShoppingListService = {
     getAllItems(knex) {
-        return knex
-            .select('*')
-            .from('shopping_list')
+      return knex
+        .select('*')
+        .from('shopping_list');
     },
     getById(knex, id) {
-        return knex
-            .from('shopping_list')
-            .select('*')
-            .where({id})
-            .first();
+      return knex
+       .from('shopping_list')
+       .select('*')
+       .where({id})
+       .first();
     },
     deleteItem(knex, id) {
-        return knex
-            .from('shopping_list')
-            .select('*')
-            .where({id})
-            .delete();
+      return knex
+        .from('shopping_list')
+        .where({ id })
+        .delete();
     },
     updateItem(knex, id, newItemFields) {
-        return knex
-            .from('shopping_list')
-            .where({id})
-            .update(newItemFields)
+      return knex
+        .from('shopping_list')
+        .where({ id })
+        .update(newItemFields);
     },
     insertItem(knex, newItem) {
-        return knex
-            .insert(newItem)
-            .into('shopping_list')
-            .returning('*')
-            .then(rows => rows[0])
+      return knex
+        .insert(newItem)
+        .into('shopping_list')
+        .returning('*')
+        .then(rows => rows[0]);
     },
-},
-
-module.exports = ShoppingListService;
+  };
+  
+  module.exports = ShoppingListService;
--- test/shopping-list-service.spec.js
+++ test/shopping-list-service.spec.js
@@ -8,28 +8,28 @@ describe(`Shopping List Service object`, function () {
       id: 1,
       name: 'First test item!',
       date_added: new Date('2029-01-22T16:28:32.615Z'),
-      price: '12.00',
+      price: '12',
       category: 'Main'
     },
     {
       id: 2,
       name: 'Second test item!',
       date_added: new Date('2100-05-22T16:28:32.615Z'),
-      price: '21.00',
+      price: '21',
       category: 'Snack'
     },
     {
       id: 3,
       name: 'Third test item!',
       date_added: new Date('1919-12-22T16:28:32.615Z'),
-      price: '3.00',
+      price: '3',
       category: 'Lunch'
     },
     {
       id: 4,
       name: 'Third test item!',
       date_added: new Date('1919-12-22T16:28:32.615Z'),
-      price: '0.99',
+      price: '1',
       category: 'Breakfast'
     },
   ];
@@ -37,7 +37,7 @@ describe(`Shopping List Service object`, function () {
   before(() => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL,
+      connection: process.env.TEST_DATABASE_URL,
     });
   });
 
@@ -129,7 +129,7 @@ describe(`Shopping List Service object`, function () {
     it(`insertItem() inserts an item and resolves it with an 'id'`, () => {
       const newItem = {
         name: 'Test new name name',
-        price: '5.05',
+        price: '5',
         date_added: new Date('2020-01-01T00:00:00.000Z'),
         checked: true,
         category: 'Lunch',
